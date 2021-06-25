908d9133197da26f092c39b37e508f80c338cdf4
MarleneContreras <86140546+MarleneContreras@users.noreply.github.com>
2021-06-24 18:09:03 -0600

    Aplicación 5
    Ejercicio de un taller mecánico  que registra las entradas y salidas de los autos, solamente hay una entrada y salida

---
+++ Talle_Mecanico/Mecanica.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Taller_mecanica
+{
+    class TALLER_ME
+    {
+        //Atributos    
+        private string[,] Autos;
+        private List<string> Registrados;
+        private List<int> cobro;
+        private int ind;
+        private int indiceS;
+        //Propiedades   
+        public string[,] Carros1
+        {
+            get => Autos; set => Autos = value;
+        }
+        public int N
+        {
+            get;
+        }
+        public List<int> Cobros
+        {
+            get => cobro; set => cobro = value;
+        }
+        public int Ind
+        {
+            get => ind; set => ind = value;
+        }
+        public List<string> CarrosRegistrados
+        {
+            get => Registrados; set => Registrados = value;
+        }
+        public int IndiceS { get => indiceS; set => indiceS = value; }
+        //Constructor    
+        public TALLER_ME(int n)
+        {
+            //se ingresa el numero de la capacidad maxima de autos 
+            N = n;
+            Autos = new string[N, 2];
+            cobro = new List<int>();
+            CarrosRegistrados = new List<string>();
+            IndiceS = 0;
+            Ind = 0;
+            for (int rows = 0; rows < N; rows++)
+            {
+                for (int columns = 0; columns < 2; columns++)
+                {
+                    Carros1[rows, columns] = " - ";
+                }
+            }
+        }
+        //metodo     
+        public Boolean Entrar(string No_placas, string Propietario)
+        {
+            if (Ind < N)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Autos[i, 0] == " - ")
+                    {
+                        Ind = i; break;
+                    }
+                }
+                Carros1[Ind, 0] = No_placas;
+                Carros1[Ind, 1] = Propietario; string value = $"{No_placas}, {Propietario}";
+                CarrosRegistrados.Add(value); return true;
+            }
+            return false;
+        }
+        public Boolean Recoger()
+        {
+            if (Ind - 1 >= 0)
+            {
+                for (int i = 0; i < N; i++)
+                {
+                    if (Autos[i, 0] != " - ")
+                    {
+                        Autos[i, 0] = Autos[i + 1, 0];
+                        Autos[i, 1] = Autos[i + 1, 1];
+                    }
+                }
+                Autos[N - 1, 0] = " - "; Autos[N - 1, 1] = " - "; return true;
+            }
+            return false;
+        }
+        public string Espacios()
+        {
+            String ans = "";
+            for (int rows = 0; rows < N; rows++)
+            {
+                ans += Autos[rows, 0] + ", "; ans += Autos[rows, 1] + "\n";
+            }
+            return ans;
+        }
+        public void Cobrar(int cobro)
+        {
+            Cobros.Add(cobro);
+        }
+        public int[] GetMonto()
+        {
+            int[] AMonto = Cobros.ToArray(); return AMonto;
+        }
+        public string[] GetCarrosAcumulados()
+        {
+            string[] ACarrosAcumulados = CarrosRegistrados.ToArray(); return ACarrosAcumulados;
+        }
+        //Propiedades   
+       
+    }
+}
+
---
+++ Talle_Mecanico/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+
+namespace Taller_mecanica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("*****TALLER MECANICO********");
+            Console.ForegroundColor = ConsoleColor.White;
+            //Capacidad maxima de 7 autos
+            TALLER_ME taller = new TALLER_ME(7);
+            Console.WriteLine(taller.Espacios());
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ESPACIOS DISPONIBLES");
+            Console.WriteLine("Al ingresar 'A' el programa se cerrara");
+            char ans; char salir = 'f';
+            while (salir != 'A')
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("¿QUE MOVIMIENTO QUIERE HACER?");
+                Console.WriteLine("A)ENTRAR\nB)RECOGER\n"); Console.WriteLine("Selecciona su opcion: ");
+                ans = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Clear(); switch (ans)
+                {
+                    case 'A':
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine("Ha seleccionado entrar");
+                            Console.Write("Introduzca su numero de placas: "); Console.ForegroundColor = ConsoleColor.White; string No_Placas = Convert.ToString(Console.ReadLine());
+                            Console.ForegroundColor = ConsoleColor.Cyan;
+                            Console.Write("Introduzca el nombre del propietario: "); Console.ForegroundColor = ConsoleColor.White; string nombre = Convert.ToString(Console.ReadLine());
+                            if (taller.Entrar( No_Placas , nombre) == false)
+                            {
+                                Console.WriteLine("Una disculpa, por el momento se encuentra lleno");
+                                Console.ReadKey();
+                            }
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                            Console.Clear();
+                            Console.WriteLine("Se han actulizado los registros");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.WriteLine(taller.Espacios());
+                            Console.ReadKey(); break;
+                        }
+                    case 'B':
+                        {
+                            Console.ForegroundColor = ConsoleColor.DarkMagenta; Console.WriteLine("Ha seleccionado recoger"); if (taller.Recoger() == false)
+                            {
+                                Console.WriteLine("Se encuentra vacio");
+                                Console.ReadKey();
+                            }
+                            Console.WriteLine("Se han actualizado los registros"); Console.ForegroundColor = ConsoleColor.White; Console.WriteLine(taller.Espacios());
+                            Console.ReadKey(); break;
+                        }
+                }
+                Console.Clear();
+                Console.WriteLine("Si ya finalizo para salir precione A o para continuar seleccione cualquier otra tecla");
+                salir = char.ToUpper(Convert.ToChar(Console.ReadLine()));
+                Console.Clear();
+            }
+        }
+    }
+}
+
